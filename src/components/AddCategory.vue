61185bc2068261749d20510b6c9b7192860a6c59
Vlad <vladyslav.pohorilyi@gmail.com>
2023-04-28 13:07:30 +0200

    added in component addproduct

--- src/components/AddCategory.vue
+++ src/components/AddCategory.vue
@@ -4,16 +4,36 @@
         <p>add category</p>
 
         <input type="text" v-model="categoryName">
-
-        <button @click="{ shop.addCattegory(categoryName);  added = !added; timers }
+    
+        <button @click=" addtoCategory
             "> add category</button>
 </div>
-<div v-if="added" class="added">
+
+<div class="error" v-if="viewError">
+        Need write name category
+    </div>
+
+<div v-if="viewSucces" class="added">
     Category {{ categoryName }}  added 
+</div>
+
+
+<div class="list">
+    <p class="title">Name category</p>
+    <div v-for="category in shop.category" >
+        {{ category.name }}
+    </div>
 </div>
 
 </template>
 <style>
+    .list .title{
+        font-weight: 700;
+        font-size: 30px;
+    }
+    .list > div{
+        margin: 10px 0;
+    }
     .product__add{
         max-width: 80%;
         margin: 0 auto;
@@ -24,7 +44,11 @@
     button,select{
         display: block;
     }
-
+.list{
+    margin-top: 20px;
+    width: 50%;
+    margin-inline: auto;
+}
 </style>
 <script>
 import { useShopStore } from '../store/Store'
@@ -41,14 +65,33 @@ export default ({
         return {
             categoryName: "",
             added: false,
+            viewError:false,
+            viewSucces:false
         };
     },
     computed: {
-        timers() {
-            setTimeout(() => { this.added = !this.added; this.categoryName = ""; }, 3000);
-        }
+        // timers() {
+        //     setTimeout(() => { this.added = !this.added; this.categoryName = ""; }, 3000);
+        // }
     },
-    components: { BackPage }
+    components: { BackPage },
+
+    methods:{
+        addtoCategory: function (){
+            if(this.categoryName == ""){
+                this.viewError = true;
+                setTimeout(()=>{this.viewError = false},2000)
+            }else{
+                this.shop.addCattegory(this.categoryName)
+                this.categoryName='';
+                this.viewSucces = true;
+                setTimeout(()=>{this.viewSucces = false}, 2000)
+                
+            }
+
+        },
+       
+    }
 })
 
 </script>
--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -21,21 +21,27 @@
         
         <label for="price">Price
 
-            <input id="price" type="number" placeholder="price" v-model="price">
+            <input id="price" type="number" placeholder="price" v-model="price" required>
 
 
         </label>
         
         <label for="description">Description
 
-            <input id="description" type="text" v-model="descProduct">
+            <input id="description" type="text" v-model="descProduct" required>
 
         </label>
+        <div class="error" v-if='viewError'>
+            Need write in all fields
+        </div>
+
+        <div class="succes" v-if='viewSucces'>
+            Good product added
+        </div>
 
 
 
-        <button @click="{ shop.addProduct(titleProduct,price,descProduct,cater); categoryName='' }
-        "> add product </button>
+        <button @click="addtoStore"> add product </button>
 
 
     </div>
@@ -73,13 +79,31 @@ export default ({
     },
     data() {
         return {
-            titleProduct: "",
+            titleProduct:'',
             price: 0,
             descProduct: "",
             cater: "",
+            viewError:false,
+            viewSucces:false,
         };
     },
-    components: { BackPage }
+    components: { BackPage },
+    methods:{
+        addtoStore: function (){
+            if(this.titleProduct =='' || this.price== 0 || this.descProduct =='' || this.cater ==''){
+                this.viewError = true;
+                setTimeout(()=>{this.viewError = false},2000)
+            }else{
+                this.shop.addProduct(this.titleProduct,this.price,this.descProduct,this.cater)
+                this.cater='';
+                this.viewSucces = true;
+                setTimeout(()=>{this.viewSucces = false}, 2000)
+                
+            }
+
+        },
+       
+    }
 })
 
 </script>
